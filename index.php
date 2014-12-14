f66272ec3c83d52746174d72ec80c29e6cb68b4d
Veselin Nikolov <veselin@automattic.com>
2014-12-14 18:11:36 +0200

    get_path will parse  and return the path

--- index.php
+++ index.php
@@ -10,13 +10,5 @@ require_once 'lib/product-index-controller.php';
 
 $db = new DB();
 
-$controller = 'index';
-if ( isset( $_GET['url'] )  ) {
-	$url_parts = explode( '/', $_GET['url'] );
-	if ( ! empty( $url_parts[0] ) ) {
-		$controller = $url_parts[0];
-	}
-}
-
 $router = new Router();
-$router->route( $controller );
+$router->route();
