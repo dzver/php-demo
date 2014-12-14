ccb5317abcc880df90112234c7292097855b49e5
Veselin Nikolov <veselin@automattic.com>
2014-12-14 17:25:07 +0200

    Add .htaccess

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once 'lib/db.php';
 $db = new DB();
 
 $products = $db->get_results( "SELECT * FROM products LIMIT 3" );
-
 foreach ( $products as $product ) {
 	?>
 		<div><span><?php echo $product->ID; ?>.</span>
