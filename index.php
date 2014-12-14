71d93f0b85849ecbc376f112f6f1664e8ed1a6ba
Veselin Nikolov <veselin@automattic.com>
2014-12-14 18:07:54 +0200

    Introduce controllers

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once 'lib/db.php';
 
 require_once 'lib/router.php';
 require_once 'config/routes.php';
+require_once 'lib/product-controller.php';
+require_once 'lib/product-index-controller.php';
 
 $db = new DB();
 
@@ -18,19 +20,3 @@ if ( isset( $_GET['url'] )  ) {
 
 $router = new Router();
 $router->route( $controller );
-
-if ( $controller == 'index' ) {
-	$products = $db->get_results( "SELECT * FROM products LIMIT 3" );
-	foreach ( $products as $product ) {
-		?>
-			<div><span><?php echo $product->ID; ?>.</span>
-				<a href="/product/<?php echo $product->ID;
-					?>"><?php echo $product->product_name; ?></a>
-			</div>
-		<?php
-	}
-}
-
-if ( $controller == 'product' ) {
-	echo 'HA!';
-}
