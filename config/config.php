04dd553db8fd025b6fc0e6eb5a6bde560585e857
Veselin Nikolov <veselin@automattic.com>
2014-12-14 17:31:28 +0200

    Add  var to index

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting( E_ALL );
+
 define( 'DB_USER', 'softuni' );
 define( 'DB_PASS', 'howdy' );
 define( 'DB_NAME', 'ecommerce' );
--- index.php
+++ index.php
@@ -5,6 +5,16 @@ require_once 'lib/db.php';
 
 $db = new DB();
 
+$controller = 'index';
+if ( isset( $_GET['url'] )  ) {
+	$url_parts = explode( '/', $_GET['url'] );
+	if ( ! empty( $url_parts[0] ) ) {
+		$controller = $url_parts[0];
+	}
+}
+
+echo $controller;
+
 $products = $db->get_results( "SELECT * FROM products LIMIT 3" );
 foreach ( $products as $product ) {
 	?>
