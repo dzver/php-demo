f62ba0fee3c7a294dbf181a48af019b8979cc209
Veselin Nikolov <veselin@automattic.com>
2014-12-14 16:51:45 +0200

    add config.php

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define( 'DB_USER', 'softuni' );
+define( 'DB_PASS', 'howdy' );
+define( 'DB_NAME', 'ecommerce' );
+define( 'DB_HOST', 'localhost' );
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-$conn = mysqli_connect( 'localhost', 'softuni', 'howdy', 'ecommerce' );
+require_once 'config/config.php';
+
+$conn = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 
 $sql = "SELECT * FROM products";
 $result = mysqli_query( $conn, $sql );
